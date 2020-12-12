32f796c3f4853a2d388cd9875b538f07ff763aa2
hardi-12 <hardi120400@gmail.com>
2020-12-12 11:19:35 +0530

    For Hosting errors1

--- EcomProj/EcomProj/settings.py
+++ EcomProj/EcomProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-0stk1cy+z1c2@j2!=k3d!^6ahx-l15@usw#g@ef%^cyxp73u6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','enhome-prj.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -120,11 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 #managing media files
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
