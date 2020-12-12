4ae7ff5267c4be3da4ce939df670416e0b470a8d
hardi-12 <hardi120400@gmail.com>
2020-12-12 10:21:52 +0530

    For Hosting

--- EcomProj/EcomProj/settings.py
+++ EcomProj/EcomProj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-0stk1cy+z1c2@j2!=k3d!^6ahx-l15@usw#g@ef%^cyxp73u6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- EcomProj/EcomProj/urls.py
+++ EcomProj/EcomProj/urls.py
@@ -20,11 +20,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.index, name='Home'),
                   path('shop/', include("shop.urls")),
                   path('',include("django.contrib.auth.urls")),
+url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
